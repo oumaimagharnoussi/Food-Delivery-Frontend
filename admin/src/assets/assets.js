b66f96b773007fe037881573b8345eec25dd3677
oumaimagharnoussi <gharnoussioumaima@gmail.com>
2025-12-13 12:18:55 +0100

    implement admin panel for managing products and orders

---
+++ admin/src/assets/assets.js
@@ -0,0 +1,22 @@
+import logo from './logo.png'
+
+import add_icon_white from './add_icon_white.png'
+import add_icon from './add_icon.png'
+
+import profile_image from './profile_image.png'
+
+import parcel_icon from './parcel_icon.png'
+import order_icon from './order_icon.png'
+import upload_area from './upload_area.png'
+export const assets = {
+    order_icon,
+    upload_area,
+    logo,
+    
+    add_icon,
+    add_icon_white,
+  profile_image,
+    parcel_icon
+}
+
+
