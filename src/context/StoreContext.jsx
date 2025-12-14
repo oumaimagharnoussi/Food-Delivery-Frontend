240b2983c16cae263e76896227f23e2e2f5b480f
oumaimagharnoussi <gharnoussioumaima@gmail.com>
2025-12-14 11:59:42 +0100

    Create user authentication (login & registration) feature

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -1,9 +1,12 @@
 import { createContext,  useState } from "react";
 import {food_list} from '../assets/assets';
+import { useEffect } from "react";
 
 export const StoreContext = createContext(null)
 const StoreContextProvider = (props) => {
   const [cartItems,setCartItems] = useState({});
+  const url = "http://localhost:4000"
+  const [token,setToken] = useState("");
   const addToCart = (itemId) => {
     if (!cartItems[itemId]) {
       setCartItems((prev)=>({...prev,[itemId]:1}))
@@ -28,13 +31,22 @@ const StoreContextProvider = (props) => {
     return totalAmount;
   }
 
+  useEffect(()=>{
+    if (localStorage.getItem("token")) {
+      setToken(localStorage.getItem("token"));
+    }
+  })
+
   const contextValue = {
      food_list,
      cartItems,
      setCartItems,
      addToCart,
      removeFromCart,
-     getTotalCartAmount
+     getTotalCartAmount,
+     url,
+     token,
+     setToken
   }
   return (
     <StoreContext.Provider value={contextValue}>
