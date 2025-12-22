3140deff6594b74f643c15b42bf983f23fdc02db
oumaimagharnoussi <gharnoussioumaima@gmail.com>
2025-12-22 12:11:43 +0100

    Fetch food data on frontend from databse

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -1,12 +1,13 @@
 import { createContext,  useState } from "react";
-import {food_list} from '../assets/assets';
 import { useEffect } from "react";
-
+import axios from "axios"
 export const StoreContext = createContext(null)
 const StoreContextProvider = (props) => {
   const [cartItems,setCartItems] = useState({});
   const url = "http://localhost:4000"
   const [token,setToken] = useState("");
+  const [food_list,setFoodList] = useState([])
+
   const addToCart = (itemId) => {
     if (!cartItems[itemId]) {
       setCartItems((prev)=>({...prev,[itemId]:1}))
@@ -31,11 +32,22 @@ const StoreContextProvider = (props) => {
     return totalAmount;
   }
 
+  const fetchFoodList = async () => {
+  const response = await axios.get(url + "/api/food/list");
+  setFoodList(response.data.date);
+};
+
+
+  
   useEffect(()=>{
+    async function loadData() {
+      await fetchFoodList();
+    
     if (localStorage.getItem("token")) {
       setToken(localStorage.getItem("token"));
     }
-  })
+  } loadData();
+  },[])
 
   const contextValue = {
      food_list,
